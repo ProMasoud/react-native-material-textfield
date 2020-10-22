f7712252b20072c6be38a6e1eaf32f10a1340f96
Masoud Tavakkoli <mtg1376@outlook.com>
2020-10-22 17:46:14 +0330

    Update index.js

--- src/components/affix/index.js
+++ src/components/affix/index.js
@@ -24,7 +24,7 @@ export default class Affix extends PureComponent {
     baseColor: PropTypes.string.isRequired,
     animationDuration: PropTypes.number.isRequired,
 
-    style: Text.propTypes,
+    style: Text.propType,
 
     children: PropTypes.oneOfType([
       PropTypes.arrayOf(PropTypes.node),
